a00e2ddd4e96379e9c3cdd6c426ed6644db57746
Łukasz Stępień <lukaszatepien@gmail.com>
2023-02-17 14:23:03 +0100

    empty window

--- Minesweeper/main.cpp
+++ Minesweeper/main.cpp
@@ -1,24 +1,23 @@
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    RenderWindow window(VideoMode(400, 600), "Minesweeper");
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+			{
+				window.close();
+			}
+			window.clear();
+			window.display();
+		}
+	}
 
     return 0;
 }
