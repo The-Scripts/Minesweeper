b1333f967d48664a8c510c9f9e98871ec6d1d448
ArcySanta <arcysanta@gmail.com>
2023-02-20 21:17:53 +0100

    now fonts working
    temporarily

--- Minesweeper/main.cpp
+++ Minesweeper/main.cpp
@@ -7,12 +7,17 @@ using namespace sf;
 
 RenderWindow window(VideoMode(480, 640), "Minesweeper", Style::Titlebar | Style::Close);
 
-void initFonts();
-
 int main()
 {
     SpriteImage timer("../Resources/icons/clock-icon.png", 1.f, 1.f, 365.f, 2.f);
 
+    // Init Fonts (temporary script)
+    Font timersFont; Font bombIndicatorFont;
+    if (!timersFont.loadFromFile("../Resources/fonts/Pixellettersfull.ttf") || !timersFont.loadFromFile("../Resources/fonts/Cabal.ttf"))
+        cout << "# The fonts did'n load! #" << endl << "# Pixellettersfull.ttf  #" << endl << "# Cabal.ttf             #" << endl;
+
+    Text test; test.setFont(timersFont); test.setFillColor(Color::Red); test.setCharacterSize(24); test.setString("Hello World!");
+    window.draw(test);//testowe linijki
 
     // Game loop
     while (window.isOpen())
@@ -30,30 +35,9 @@ int main()
         window.clear(Color(45, 45, 45, 255));
         window.draw(timer.sprite);
         window.display();
-        initFonts();
+        
     }
-
     
     // End of the app
-
     return 0;
-}
-
-//Function initiating fonts
-void initFonts() {
-
-    Font timersFont; Font bombIndicatorFont;
-    if (!timersFont.loadFromFile("../Resources/fonts/Pixellettersfull.ttf"))
-    {
-        cout << "# The font did'n load! #" << endl;
-        cout << "# Pixellettersfull.ttf #" << endl;
-    }
-    if (!timersFont.loadFromFile("../Resources/fonts/Cabal.ttf"))
-    {
-        cout << "# The font did'n load! #" << endl;
-        cout << "# Cabal.ttf            #" << endl;
-    }
-    Text test; test.setFont(timersFont); test.setFillColor(Color::Red); test.setCharacterSize(24); test.setString("Hello World!"); 
-    window.draw(test);
-
 }
