b6c628f376157c763cc8b891084d5c034a1130d3
Łukasz Stępień <lukaszatepien@gmail.com>
2023-03-01 18:09:51 +0100

    Create empty methods

---
+++ Minesweeper/UncoverEmpty.cpp
@@ -0,0 +1,33 @@
+#include "UncoverEmpty.h"
+
+void UncoverEmpty::top(short& cells, short pos)
+{
+}
+
+void UncoverEmpty::upperRight(short& cells, short pos)
+{
+}
+
+void UncoverEmpty::right(short& cells, short pos)
+{
+}
+
+void UncoverEmpty::lowerRight(short& cells, short pos)
+{
+}
+
+void UncoverEmpty::buttom(short& cells, short pos)
+{
+}
+
+void UncoverEmpty::lowerLeft(short& cells, short pos)
+{
+}
+
+void UncoverEmpty::left(short& cells, short pos)
+{
+}
+
+void UncoverEmpty::upperLeft(short& cells, short pos)
+{
+}
--- Minesweeper/UncoverEmpty.h
+++ Minesweeper/UncoverEmpty.h
@@ -1,5 +1,18 @@
 #pragma once
 class UncoverEmpty
 {
-};
+public:
+	// Contructor / Desctructor
+	UncoverEmpty(short& cells, short pos);
+	~UncoverEmpty();
 
+	// Methods
+	void top(short& cells, short pos);
+	void upperRight(short& cells, short pos);
+	void right(short& cells, short pos);
+	void lowerRight(short& cells, short pos);
+	void buttom(short& cells, short pos);
+	void lowerLeft(short& cells, short pos);
+	void left(short& cells, short pos);
+	void upperLeft(short& cells, short pos);
+};
